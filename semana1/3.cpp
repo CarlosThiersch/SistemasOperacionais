3bf97f8168ea13e1d2ba46da92481921e1318e8e
RenatoFleury <renatonavesfleury@gmail.com>
2024-07-15 10:19:02 -0300

    correção das impressoes de saída do exercício 3.

--- semana1/3.cpp
+++ semana1/3.cpp
@@ -2,16 +2,6 @@
 gcc -pthread ... 
 */
 
-/*
-Construa um programa que execute 𝑛 threads. Cada thread deve realizar a tarefa definida 
-pela função worker. Todas as threads devem ser exclusivamente orientadas à CPU.
-
-(Opcional para análise) Crie a versão sequencial desse mesmo programa, isto é, a função
-worker é executada 𝑛 vezes sequencialmente. Compare os tempos de execução do
-programa usando threads e a versão sequencial.
-
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -23,9 +13,10 @@ programa usando threads e a versão sequencial.
 int iteracoes = 1000;
 
 void* worker(void* thread_id){
+	printf("\tCriando thread %ld.\n",(long)thread_id);
 	clock_t t;
 	t = clock();
-	printf("\tCriando thread %ld\t(clock=%.3lf)\n",(long)thread_id,(double)t);
+	// printf("*** teste: endereço de t = %ld\n",(long)&t);
 	for (int i=0; i<iteracoes; i++){
 		for (int j=0; j<iteracoes; j++){
 			for (int k=0; k<iteracoes; k++){
@@ -34,7 +25,7 @@ void* worker(void* thread_id){
 		}
 	}
 	t = clock() - t;
-	printf("\tFinalizando thread %ld\t(delta clock=%.3lf)\n", (long)thread_id,(double)t);
+	printf("\tThread %ld finalizada.\t(Tempo de execução=%.3lf)\n", (long)thread_id,(double)t/CLOCKS_PER_SEC);
 	pthread_exit(NULL);
 }
 
@@ -43,12 +34,26 @@ int main(int argc, char * argv[]){
 	int n_threads = atol(argv[1]);
 	pthread_t threads[n_threads];
 	
+	printf("\nTeste de threads concorrentes:\n");
+	clock_t t1 = clock();
     for (long t = 0; t < n_threads; t++) {
         pthread_create(&threads[t], NULL, worker, (void*)t);
     }
+
 	for (int t = 0; t < n_threads; t++) {
 		pthread_join(threads[t], NULL);
 	}
+	t1 = clock() - t1;
+	printf("Tempo total do teste=%.3lf\n",(double)t1/CLOCKS_PER_SEC);
+
+	clock_t t2 = clock();
+	printf("\nTeste de threads sequenciais:\n");
+	for (long t = 0; t < n_threads; t++) {
+        pthread_create(&threads[t], NULL, worker, (void*)t);
+		pthread_join(threads[t], NULL);
+    }
+	t2 = clock() - t2;
+	printf("Tempo total do teste=%.3lf.\n",(double)t2/CLOCKS_PER_SEC);
 	
 	return 0;
 }
